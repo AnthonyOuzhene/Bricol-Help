63cac7b74e0a0374f17303cfa3169723070eaf9e
AnthonyOuzhene <a.ouzhene@gmail.com>
2022-06-13 10:49:56 +0200

    trim on api for content & rights given ot user for uploads images

--- wp-content/plugins/bricolhelp/Api/Tutorials.php
+++ wp-content/plugins/bricolhelp/Api/Tutorials.php
@@ -4,7 +4,8 @@ namespace bricolHelp\Api;
 
 use bricolHelp\PostType\TutorialsPostType;
 
-class Tutorials {
+class Tutorials
+{
 
     /**
      * run()
@@ -32,14 +33,14 @@ class Tutorials {
         // dans $response, on trouve la propriété "data" qui est un array associatif dans lequel on peut placer des données
         // wp_trim_excerpt() => sans argument précisé, cette fonction récupère l'extrait pour le post courant (en le générant depuis le contenu si besoin). Dans ce filtre, chaque post utilisé pour gérer la requête
         // strip_tags() pour supprimer le HTML qui pourrait s'y trouver
-        
+
         $response->data['excerpt'] = strip_tags(wp_trim_excerpt());
+        $response->data['content'] = strip_tags(wp_trim_excerpt());
 
         // on ajoute l'url du thumbnail => image mise en avant, dans un format réduit
         $response->data['thumbnail'] = get_the_post_thumbnail_url();
-        
+
         // on est dans un filter, on doit donc retourner une valeur
         return $response;
     }
-
 }
